6366f46b8c739669ff6bcacbaee0b2fb7dd89a28
SaidiAmine <amine.saidi@esprit.tn>
2022-01-17 15:00:49 +0100

    initial commit

---
+++ src/main/java/com/saidi/tutorialmanagementbe/TutorialManagementBeApplication.java
@@ -0,0 +1,13 @@
+package com.saidi.tutorialmanagementbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialManagementBeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TutorialManagementBeApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/saidi/tutorialmanagementbe/TutorialManagementBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saidi.tutorialmanagementbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialManagementBeApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
